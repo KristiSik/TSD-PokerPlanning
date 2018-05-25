f737cb94576988a1a9d5d572c4769a013500e452
Kristian Sik <krissik98@yandex.ua>
2018-05-25 15:09:40 +0200

    - data seeder added

---
+++ PlanningPokerBackend/Models/DataSeeder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanningPokerBackend.Models
+{
+    public class DataSeeder
+    {
+        private readonly PlanningPokerDbContext _context;
+        public DataSeeder(PlanningPokerDbContext context)
+        {
+            _context = context;
+        }
+        public void SeedData()
+        {
+            if (_context.Users.Count() != 0)
+            {
+                return;
+            }
+            List<User> users = new List<User>() {
+                new User() { FirstName = "Dave", LastName = "Murray", Email = "davemurray@mail.com", Password = "encrypted_password" },
+                new User() { FirstName = "Adrian", LastName = "Smith", Email = "adriansmith@mail.com", Password = "my_password" },
+                new User() { FirstName = "Steve", LastName = "Harris", Email = "steveharris@mail.com", Password = "my_password" },
+                new User() { FirstName = "Janick", LastName = "Gers", Email = "janickgers@mail.com", Password = "my_password" },
+                new User() { FirstName = "Nick", LastName = "McBrain", Email = "nickmcbrain@mail.com", Password = "my_password" },
+                new User() { FirstName = "Bruce", LastName = "Dickinson", Email = "brucedickinson@mail.com", Password = "my_password" }
+            };
+            PlayTable playTable = new PlayTable() { Admin = users.First(), Participants = users.Skip(1).ToList(), Token = "abcdef" };
+            _context.Users.AddRange(users);
+            _context.PlayTables.Add(playTable);
+            _context.SaveChanges();
+        }
+    }
+}
--- PlanningPokerBackend/Startup.cs
+++ PlanningPokerBackend/Startup.cs
@@ -31,13 +31,10 @@ namespace PlanningPokerBackend
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetRequiredService<PlanningPokerDbContext>();
-                try
-                {
-                    context.Database.Migrate();
-                }
-                catch (Exception e)
-                {
-                }
+                context.Database.Migrate();
+                
+                DataSeeder ds = new DataSeeder(context);
+                ds.SeedData();
             }
 
             app.UseMvc(routes =>
