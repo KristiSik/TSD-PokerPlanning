a74e66670def03de77679911f91e68d3d568a5a3
Kristian Sik <krissik98@yandex.ua>
2018-06-08 21:19:47 +0200

    - fixing migration

--- PlanningPokerBackend/Startup.cs
+++ PlanningPokerBackend/Startup.cs
@@ -31,8 +31,8 @@ namespace PlanningPokerBackend
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetRequiredService<PlanningPokerDbContext>();
-                // context.Database.Migrate();
                 context.Database.EnsureCreated();
+                context.Database.Migrate();
               
                 DataSeeder ds = new DataSeeder(context);
                 ds.SeedData();
